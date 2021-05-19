a1f7cfcab69f55d4de7b080fcba2f5c26e40e11b
Cooplix <vadikdeputat@gmail.com>
2021-05-19 16:23:28 +0200

    4 files modified

--- src/app/task-list/task-list.component.ts
+++ src/app/task-list/task-list.component.ts
@@ -2,20 +2,15 @@ import { style } from '@angular/animations';
 import { Input, Output, EventEmitter, Component, OnInit } from '@angular/core';
 import { Task } from '../task';
 
-
 @Component({
   selector: 'app-task-list',
   templateUrl: './task-list.component.html',
   styleUrls: ['./task-list.component.css']
 })
-
 export class TaskListComponent implements OnInit {
+  constructor() {}
 
- 
-  constructor() { }
-
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
   @Input()
   tasks: Task[];
@@ -24,16 +19,14 @@ export class TaskListComponent implements OnInit {
   remove = new EventEmitter<number>();
 
   removeTask(taskIdx: number) {
-    if(window.confirm('Are sure you want to delete this item?')) {
-        this.remove.emit(taskIdx);
+    if (window.confirm('Are sure you want to delete this item?')) {
+      this.remove.emit(taskIdx);
     }
   }
 
   doneTask(taskIdx: number) {
     this.tasks.forEach(task => {
-        if(task.id == taskIdx) task.completed = true;
-    })   
-}
-
-
+      if (task.id == taskIdx) task.completed = true;
+    });
+  }
 }
